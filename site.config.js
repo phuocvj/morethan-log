5710217c53eab6e85c0a191784bb707e2ca16508
Lê Thiện Phước <lebaokhaky@gmail.com>
2024-03-08 16:01:30 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -57,7 +57,7 @@ const CONFIG = {
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
-      label: "💬 Utterances",
+      label: "💬 Comment",
     },
   },
   cusdis: {
