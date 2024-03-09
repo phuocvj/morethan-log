6af8f65f6034153dd35f5cad38cb579c2d68ad2e
Lê Thiện Phước <lethienphuoc@outlook.com>
2024-03-09 10:09:06 +0700

    Update Profile Name.

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "phuocvj",
+    name: "Lê Thiện Phước",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend & backend developer",
     bio: "I develop everything using Database (Oracle, SQL Server, MySQL, Postgre,...), C#, node, nextjs, reactjs.",
